673762c788096308a40c20e262f704463f5624e2
Kade Griffith <kadeg95@gmail.com>
2021-09-17 10:28:45 -0600

    authentication system is set up. Correct credentials will allow user to access /bubbles which is routed privately. Going to move on to working on the bubbles page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage'
+
+import PrivateRoute from "./components/PrivateRoute";
+import axiosWithAuth from "./helpers/axiosWithAuth";
+
 import "./styles.scss";
 
 function App() {
+
+
+  const logout = () => {
+    axiosWithAuth()
+    .post('/logout')
+    .then(res => {
+      localStorage.removeItem('token')
+      window.location.href = '#'
+    })
+  }
+
   return (
     <Router>
-      <div className="App">
-        <header>
-          Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
-        </header>
-      </div>
+      <Switch>
+        <div className="App">
+          <header>
+            Color Picker Sprint Challenge
+            <a onClick={logout} data-testid="logoutButton" href="#">logout</a>
+          </header>
+          <PrivateRoute path='/bubbles' component={BubblePage}/>
+          <PrivateRoute path="/logout" />
+          <Route path="/login" component={Login}/>
+          <Route exact path="/" component={Login}/>
+        </div>
+      </Switch>
     </Router>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,65 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-const Login = () => {
+const initialState ={
+  username: '',
+  password:''
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
-  //replace with error state
+  const [credentials, setCredentials] = useState(initialState)
+  const [error, setError] = useState(null)
+
+  const { history } = props;
+
+  const handleError = () => {
+    if(credentials.username !== 'Lambda' || credentials.password !== 'School'){
+      setError('Invalid username or password')
+    } else {
+      setError(null);
+    }
+  }
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+    console.log(credentials)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:5000/api/login', credentials)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem('token', res.data.payload)
+      history.push('/bubbles')
+    })
+    .catch(err => console.log(err))
+  }
+    
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <label>
+            UserName:
+            <input onChange={handleChange} id='username' name='username' type= 'text' value={credentials.username} ></input>
+          </label>
+          <label>
+            Password:
+            <input onChange={handleChange} id='password' name='password' type= 'password' value={credentials.password} ></input>
+          </label>
+          <button onClick={handleError} type='submit' id='submit'>Log in</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
